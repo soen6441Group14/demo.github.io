9b22ef4c259d05ce07a577d4f8d84ef68c132d81
ShahramAr <s.aryanpour90@gmail.com>
2017-02-02 15:05:49 -0500

    3rd commit

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("Hello world!");
+		System.out.println("3rd commit");
 		//test
 	}
 }
