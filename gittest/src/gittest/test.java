9ead87a99b10511a4fa4d09e35b2c09dd6cd9208
ShahramAr <s.aryanpour90@gmail.com>
2017-02-02 14:58:18 -0500

    2nd commit

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -3,7 +3,7 @@ package gittest;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("Hello!");
+		System.out.println("Hello world!");
 		//test
 	}
 }
